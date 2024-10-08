51e4a07f14ab071ab32735d2d8d1c6423295b979
mani80624 <manueldolores01@gmail.com>
2024-10-07 20:56:23 -0600

    Agregando un div de edición de cursos

--- octopus/roles/administrador/curses.php
+++ octopus/roles/administrador/curses.php
@@ -10,47 +10,47 @@ if($_SESSION['name']==null || $_SESSION['name']==''){
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Courses</title>
     <meta name="keywords" content="HTML5 Admin Template" />
-		<meta name="description" content="JSOFT Admin - Responsive HTML5 Template">
-		<meta name="author" content="JSOFT.net">
+	<meta name="description" content="JSOFT Admin - Responsive HTML5 Template">
+	<meta name="author" content="JSOFT.net">
 
-		<!-- Mobile Metas -->
-		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+	<!-- Mobile Metas -->
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
 
-		<!-- Web Fonts  -->
-		<link href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800|Shadows+Into+Light" rel="stylesheet" type="text/css">
+	<!-- Web Fonts  -->
+	<link href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800|Shadows+Into+Light" rel="stylesheet" type="text/css">
 
-		<!-- Vendor CSS -->
-		<link rel="stylesheet" href="/octopus/assets/vendor/bootstrap/css/bootstrap.css" />
-		<link rel="stylesheet" href="/octopus/assets/vendor/font-awesome/css/font-awesome.css" />
-		<link rel="stylesheet" href="/octopus/assets/vendor/magnific-popup/magnific-popup.css" />
-		<link rel="stylesheet" href="/octopus/assets/vendor/bootstrap-datepicker/css/datepicker3.css" />
+	<!-- Vendor CSS -->
+	<link rel="stylesheet" href="/octopus/assets/vendor/bootstrap/css/bootstrap.css" />
+	<link rel="stylesheet" href="/octopus/assets/vendor/font-awesome/css/font-awesome.css" />
+	<link rel="stylesheet" href="/octopus/assets/vendor/magnific-popup/magnific-popup.css" />
+	<link rel="stylesheet" href="/octopus/assets/vendor/bootstrap-datepicker/css/datepicker3.css" />
 
-		<!-- Specific Page Vendor CSS -->
-		<link rel="stylesheet" href="/octopus/assets/vendor/jquery-ui/css/ui-lightness/jquery-ui-1.10.4.custom.css" />
-		<link rel="stylesheet" href="/octopus/assets/vendor/bootstrap-multiselect/bootstrap-multiselect.css" />
-		<link rel="stylesheet" href="/octopus/assets/vendor/morris/morris.css" />
+	<!-- Specific Page Vendor CSS -->
+	<link rel="stylesheet" href="/octopus/assets/vendor/jquery-ui/css/ui-lightness/jquery-ui-1.10.4.custom.css" />
+	<link rel="stylesheet" href="/octopus/assets/vendor/bootstrap-multiselect/bootstrap-multiselect.css" />
+	<link rel="stylesheet" href="/octopus/assets/vendor/morris/morris.css" />
 
-		<!-- Theme CSS -->
-		<link rel="stylesheet" href="/octopus/assets/stylesheets/theme.css" />
+	<!-- Theme CSS -->
+	<link rel="stylesheet" href="/octopus/assets/stylesheets/theme.css" />
 
-		<!-- Skin CSS -->
-		<link rel="stylesheet" href="/octopus/assets/stylesheets/skins/default.css" />
+	<!-- Skin CSS -->
+	<link rel="stylesheet" href="/octopus/assets/stylesheets/skins/default.css" />
 
-		<!-- Theme Custom CSS -->
-		<link rel="stylesheet" href="/octopus/assets/stylesheets/theme-custom.css">
+	<!-- Theme Custom CSS -->
+	<link rel="stylesheet" href="/octopus/assets/stylesheets/theme-custom.css">
 
-		<!-- Custom css -->
-		<link rel="stylesheet" href="/octopus/roles/administrador/css/styles.css">
+	<!-- Custom css -->
+	<link rel="stylesheet" href="/octopus/roles/administrador/css/styles.css">
 
-		<!-- Head Libs -->
-		<script src="/octopus/assets/vendor/modernizr/modernizr.js"></script>
+	<!-- Head Libs -->
+	<script src="/octopus/assets/vendor/modernizr/modernizr.js"></script>
 </head>
 <body>
 		<section class="body">
 			<!-- start: header -->
 				<?php include 'includes_header.php'; ?> 
 			<!-- end: header -->
-		<div class="inner-wrapper" id="body">
+		<div class="inner-wrapper">
 			<?php include 'includes_bar_lat.php';?>
 			<section role="main" class="content-body">
 			  <div class="row">
@@ -79,14 +79,46 @@ if($_SESSION['name']==null || $_SESSION['name']==''){
 						</div>
 					</div>
 				</div>
-				
+
+				<div class="row mt4" id="hide-edit">
+					<div class="tabs">
+						<div class="tab-content">
+							<div class="tab">
+								<form class="form horizontal">
+									<h4 class="mb-xlg">Editar</h4>
+									<fieldset>
+										<div class="form-group">
+											<label class="col-md-3 control-label">Nombre del curso</label>
+											<div class="col-md-8">
+												<input type="text" name="name_course" id="name_course" class="form-control">
+											</div>
+										</div>
+										<div class="form-group">
+											<label class="col-md-3 control-label">Horas del curso</label>
+											<div class="col-md-8">
+												<input type="number" name="hours" id="hours" class="form-control">
+											</div>
+										</div>
+										<div class="form-group">
+											<label class="col-md-3 control-label">Descripción de curso (no más de 250 carácteres)</label>
+											<div class="col-md-8">
+												<input type="text" name="description" id="description" class="form-control">
+											</div>
+										</div>
+									</fieldset>
+								</form>
+							</div>
+						</div>
+					</div>
+				</div>
+
 
 				<div class="row">
 					<button class="btn btn-primary" id="button-register">Crear nuevo curso</button>
-			  </div>
-			  
-			  <div class="row mt-4" id="register-course">
-			  <div class="tabs">
+			 	 </div>
+
+			  	<div class="row mt-4" id="register-course">
+			  		<div class="tabs">
 						<div class="tab-content">
 							<div class="tab">
 								<form id="courses_form" class="form horizontal">
